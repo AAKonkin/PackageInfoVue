c0d44484de75bb8d682d48df0e5a617b11f62a27
AAKonkin <konich1999@yandex.ru>
2023-03-31 22:42:29 +0300

    Problems with GH-Pages#1

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,7 @@
-// const { defineConfig } = require('@vue/cli-service')
-// module.exports = defineConfig({
-//   publicPath: process.env.NODE_ENV === 'production' ?
-//   '/packinfovue/' :
-//   '/',
-//   transpileDependencies: true
-// })
-module.exports = {
-  publicPath: '/packinfovue/'
-}
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
+  publicPath: process.env.NODE_ENV === 'production' 
+  ? '/packinfovue/' 
+  : '/',
+  //transpileDependencies: true
+})
